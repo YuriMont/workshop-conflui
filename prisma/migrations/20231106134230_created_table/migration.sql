a48274cd85977cd19e6a7312493b88faaf1c53a6
YuriMont <yuri.mont1043@gmail.com>
2023-11-06 14:28:38 -0300

    feet: prism configuration

---
+++ prisma/migrations/20231106134230_created_table/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `Users` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+INSERT INTO "new_Users" ("email", "id", "name") SELECT "email", "id", "name" FROM "Users";
+DROP TABLE "Users";
+ALTER TABLE "new_Users" RENAME TO "Users";
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
